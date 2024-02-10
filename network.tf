6a36e2783b77c10f478b179727395e4469fb93ae
Ramon Risério - DevOps Engineer <ramonriserio@hotmail.com>
2024-02-10 00:57:29 +0000

    Criação de recursos de rede com Terraform (VPC, subnets, IGW, route table e security group)

--- network.tf
+++ network.tf
@@ -10,14 +10,16 @@ resource "aws_vpc" "main" {
 # Adicione duas subredes à VPC criada
 resource "aws_subnet" "subnet" {
     vpc_id = aws_vpc.main.id
-    cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+#    cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+    cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = true
     availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "subnet2" {
     vpc_id = aws_vpc.main.id
-    cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8 , 2)
+#    cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8 , 2)
+    cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = true
     availability_zone = "us-east-1b"
 }
